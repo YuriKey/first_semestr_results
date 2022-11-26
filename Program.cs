ffacd503b43a6930938ca647d3d70943024f1207
yuri_k <krenev_yu@mail.ru>
2022-11-26 13:01:54 +0400

    final_code

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿int CountArray(string[] Array, int Sym)
+{
+    int res = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= Sym)
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+string[] ReduceArray(string[] Array, int Sym, int sizeNewArray)
+{
+    string[] ResultArray = new string[sizeNewArray];
+    int j = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= Sym)
+        {
+            ResultArray[j] = Array[i];
+            j++;
+        }
+    }
+
+    return ResultArray;
+}
+
+Console.Clear();
+Console.WriteLine("Введите данные через запятую и пробел: ");
+string row = Console.ReadLine()!;
+Console.Write("Введите искомое количество символов: ");
+int N = int.Parse(Console.ReadLine()!);
+
+string[] nums = row.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+int size = CountArray(nums, N);
+
+Console.WriteLine();
+Console.Write($"Массив с элементами меньше {N} символов: [");
+Console.Write(String.Join(", ", ReduceArray(nums, N, size)));
+Console.Write("]");
